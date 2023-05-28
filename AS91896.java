e504edaadf15f21182f9ba7979e1322cd08f259a
finlahe <134564885+finlahe@users.noreply.github.com>
2023-05-29 10:33:32 +1200

    working on a boolean

--- AS91896.java
+++ AS91896.java
@@ -4,10 +4,11 @@
  *
  * @author (Helene)
  * @Start Date (23/5/23)
- * @Version Date (26/5/23)
- * @version (2)
+ * @Version Date (29/5/23)
+ * @Version (3)
  * I'm creatting conways game of life
  */
+//2d buillin if statmnt if true x in not 0
 import java.util.Scanner;
 public class AS91896
 {
@@ -19,6 +20,7 @@ public class AS91896
         //Variables
         int arrySize = 0;
         int cellSize = 0;
+        boolean[][] cellHasLife;
         System.out.println("Conways Game Of Life");
         
         //Player picking game size/array size
@@ -26,6 +28,24 @@ public class AS91896
         Scanner playerSize;
         playerSize= new Scanner(System.in);
         arrySize=playerSize.nextInt();
+        //deffine boolean arry
+        
+        cellHasLife = new boolean[arrySize][arrySize];
+        
+        
+        //Player picking how many cells to start with
+        System.out.println("Pick How Many Cell's To start Off With");
+        Scanner cellStartSize;
+        cellStartSize= new Scanner(System.in);
+        cellSize=cellStartSize.nextInt();
+        
+        //Boolean printing true or false
+        if (cellHasLife=true) System.out.print("Ture");
+        else System.out.print("False");
+            
+        
+        
+    
         //The array
         for (int x=0;x<arrySize;x++) {
             /*System.out.print(x+" ");*/

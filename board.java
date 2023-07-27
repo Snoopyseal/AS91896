6f8206e109e9a89f5bd95235f65a6718506ef2ed
finlahe <134564885+finlahe@users.noreply.github.com>
2023-07-27 13:07:16 +1200

    Clearing and lopping
    lopping the main body so i can see the cells become alive, and clear the screen so it doesn't get overrun

--- board.java
+++ board.java
@@ -22,6 +22,8 @@ public class board
         Random rand = new Random();
         
         //Variables
+        //counter is for testing purposes
+        int counter = 0;
         int arraySize = 0;
         int cellSize = 0;
         boolean[][] cellHasLife;
@@ -41,7 +43,6 @@ public class board
         
         
         
-        
         //deffine boolean arry for the cell has life
         cellHasLife = new boolean[arraySize][arraySize];
         
@@ -54,64 +55,76 @@ public class board
         }
         
         // genarteing the array
-        for (int x=0;x <arraySize;x++){
-            for (int y=0;y <arraySize;y++){
-                // checking what cells are true and false
-                if (cellHasLife[x][y]==true){
-                    //System.out.print("True"); 
-                    System.out.print("⬛");
-                    
-                    }
-                else {
-                    //System.out.print("False");
-                    System.out.print("⬜");
-                }
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-        //checking for life
-        //debuging
-        for(int x=0;x <arraySize;x++){
-            for (int y=0;y <arraySize;y++){
-                //making sure that we aren't checking out size or the arrays size
-                if(x<arraySize-1){
-                    //cheking though agreasnet cells, if the cell bellow me is true it'd printing out
-                        if (cellHasLife[x+1][y]==true){
-                         // if (x<arraySize||y<arraySize){ 
-                        cellHasLife[x][y]= true;
-                        System.out.print("cell bellow is alive "+x+","+y);
-                        // }
+            while  (counter<30)
+            {
+            System.out.println(counter);
+            counter++;
+            //celaring the screen  
+            System.out.print('\u000C');
+            
+            for (int x=0;x <arraySize;x++){
+                for (int y=0;y <arraySize;y++){
+                    // checking what cells are true and false
+                    if (cellHasLife[x][y]==true){
+                        //System.out.print("True"); 
+                        System.out.print("⬛");
                         
-                           System.out.println(" ");
                         }
                     else {
-                    //System.out.print("False");
-                    // System.out.print(" ");
+                        //System.out.print("False");
+                        System.out.print("⬜");
                     }
-                    // System.out.print(" ");
-                    }
-                
-                if(y<arraySize-1){
-                    //cheking though agreasnet cells, if the cell bellow me is true it'd printing out
-                        if (cellHasLife[x][y+1]==true){
-                         // if (x<arraySize||y<arraySize){ 
-                        System.out.print("cell next to is alive "+x+","+y);
-                        // }
-                           System.out.println(" ");
-                        }
-                    else {
-                    //System.out.print("False");
-                    // System.out.print(" ");
-                    }
-                    // System.out.print(" ");
-                    }
+                    System.out.print(" ");
+                }
+                System.out.println();
             }
             System.out.println();
-        }
-        //print random intergers
-        
+            //checking for life
+            //debuging
+            for(int x=0;x <arraySize;x++){
+                for (int y=0;y <arraySize;y++){
+                    //making sure that we aren't checking out size or the arrays size
+                    if(x<arraySize-1){
+                        //cheking though agreasnet cells, if the cell bellow me is true it'd printing out
+                            if (cellHasLife[x+1][y]==true){
+                             // if (x<arraySize||y<arraySize){ 
+                                 //trying to change if there are two next to that are alive in any directio
+                                cellHasLife[x][y]= true;
+                            System.out.print("cell bellow is alive "+x+","+y);
+                            // }
+                            
+                               System.out.println(" ");
+                            }
+                        else {
+                        //System.out.print("False");
+                        // System.out.print(" ");
+                        }
+                        // System.out.print(" ");
+                        }
+                    
+                    if(y<arraySize-1){
+                        //cheking though agreasnet cells, if the cell bellow me is true it'd printing out
+                            if (cellHasLife[x][y+1]==true){
+                             // if (x<arraySize||y<arraySize){ 
+                            System.out.print("cell next to is alive "+x+","+y);
+                                cellHasLife[x][y]= true;
+                            // }
+                               System.out.println(" ");
+                            }
+                        else {
+                        //System.out.print("False");
+                        // System.out.print(" ");
+                        }
+                        // System.out.print(" ");
+                        }
+                    
+                        
+                    
+                }
+                System.out.println();
+            }
+            //print random intergers
+            
+        }  
     }
-
-}
+}   
